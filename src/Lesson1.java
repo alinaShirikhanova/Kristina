6faa9fe0d3ebf8b56082b1899d58f157b43d1c09
alina <alina.meller.98@mail.ru>
2025-06-20 13:42:19 +0300

    lesson1

---
+++ src/Lesson1.java
@@ -0,0 +1,24 @@
+public class Lesson1 {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        System.out.println("Hello my friends");
+//        System.out.println("Hello world!");
+//        System.out.print("Hello my friends");
+//        Однострочный (Первый вид комментариев)
+        /*
+         * Многострочный комментарий
+         * Многострочный комментарий
+         * Многострочный комментарий
+         * Многострочный комментарий
+         */
+//        System.out.print("Hello world!\nHello my friends!");
+        System.out.println(5 + 5);
+        // Конкатенация
+        System.out.println("5" + "5");
+        System.out.println("Результат: " + 10);
+        System.out.println("Результат: " + (10 + 5));
+    }
+}
+{
+}
