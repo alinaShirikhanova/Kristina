2117c96e14ef764289dd805f376293dcd00b27da
alina <alina.meller.98@mail.ru>
2025-06-27 19:00:11 +0300

    lesson2

---
+++ src/Lesson2.java
@@ -0,0 +1,29 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+//        print line
+//        System.out.print("Hello world!");
+//        System.out.print("Hello world!");
+//        System.out.println("Добро пожаловать в Java!");
+
+//        System.out.println("Учеба");
+//        System.out.println("- это весело!");
+
+        System.out.println(10 + 10);
+//        System.out.println(10 - 10);
+//        System.out.println(10 * 10);
+//        System.out.println(10 / 3);
+//        System.out.println(10.9 / 3);
+        // Остаток от деления
+        System.out.println(5 % 2);
+
+
+//        Гостей: 5 человек
+//        Конфет: 23 штуки
+
+//        System.out.println(23 % 5);
+
+//        System.out.println(2 / (3 + 4) * 5);
+
+//        "1/ dkfkjdfkjskdffdj\ndjksjfkfjksdjf"
+    }
+}
