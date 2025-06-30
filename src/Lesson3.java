d1be295ca36c20a9bb31f066d601bb90e47d9efd
alina <alina.meller.98@mail.ru>
2025-06-30 22:52:26 +0300

    lesson3

---
+++ src/Lesson3.java
@@ -0,0 +1,48 @@
+public class Lesson3 {
+    public static void main(String[] args) {
+
+        // Объявление переменной
+        byte n;
+//        System.out.println(n); Ошибка, потому что в ней еще ничего не лежит
+        // Инициализация - присвоение первого значения
+        n = 127;
+        System.out.println(n);
+// Объявление и инициализация
+        byte b = 90;
+//        byte a, c;
+//        byte a = 89, c;
+        byte a = 89, c;
+//        System.out.println(number); Ошибка (такой переменной не существует)
+//        age
+//        userName
+//        имя
+//        imya
+        System.out.println(a);
+
+        a = 10 + 5;
+        System.out.println(a);
+
+        short y = 89;
+
+        int r = 2000000000;
+
+        byte first = 1;
+        byte second = 1;
+        int res = first + second;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
