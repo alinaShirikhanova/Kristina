a373c56088df118788af0b2fc30fab9c1b740432
alina <alina.meller.98@mail.ru>
2025-07-11 14:55:02 +0300

    lesson4

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,34 +1,63 @@
+import java.util.Scanner;
+
 public class Lesson3 {
     public static void main(String[] args) {
 
-        // Объявление переменной
-        byte n;
-//        System.out.println(n); Ошибка, потому что в ней еще ничего не лежит
-        // Инициализация - присвоение первого значения
-        n = 127;
-        System.out.println(n);
-// Объявление и инициализация
-        byte b = 90;
-//        byte a, c;
+//        // Объявление переменной
+//        byte n;
+////        System.out.println(n); Ошибка, потому что в ней еще ничего не лежит
+//        // Инициализация - присвоение первого значения
+//        n = 127;
+//        System.out.println(n);
+//// Объявление и инициализация
+//        byte b = 90;
+////        byte a, c;
+////        byte a = 89, c;
 //        byte a = 89, c;
-        byte a = 89, c;
-//        System.out.println(number); Ошибка (такой переменной не существует)
-//        age
-//        userName
-//        имя
-//        imya
-        System.out.println(a);
+////        System.out.println(number); Ошибка (такой переменной не существует)
+////        age
+////        userName
+////        имя
+////        imya
+//        System.out.println(a);
+//
+//        a = 10 + 5;
+//        System.out.println(a);
+//
+//        short y = 89;
+//
+//        int r = 2000000000;
+//
+//        byte first = 1;
+//        byte second = 1;
+//        int res = first + second;
+//        byte - 1 байта
+//        short - 2 байта
+//        int - 5 байта
+//        long - 8 байт
 
-        a = 10 + 5;
-        System.out.println(a);
+//        long l = 2000000000000L;
 
-        short y = 89;
+//        long l = 1000000000000L;
 
-        int r = 2000000000;
 
-        byte first = 1;
-        byte second = 1;
-        int res = first + second;
+//        int c = подставь то, что пользователь введет;
+//        int s = 5;
+//        System.out.println(12 + 5);
+
+
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/Lesson4.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+        //        long l = 2000000000000L;
+
+        long l = 1000000000000L;
+//        System.out.println(20000000000L);
+
+
+//        int c = подставь то, что пользователь введет;
+//        int s = 5;
+//        System.out.println(12 + 5);
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(sc.nextInt());
+
+
+        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(sc.nextInt());
+//        System.out.println(a + b);
+//        System.out.println(a * b);
+
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        System.out.println(a + b + c);
+//        System.out.println(a - b - c);
+
+        int s = sc.nextInt();
+        int l = sc.nextInt();
+        System.out.println(s / l + " км/ч");
+    }
+}
